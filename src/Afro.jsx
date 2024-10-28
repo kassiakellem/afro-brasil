36c7200c277754bf9777a24b0daee7fc26416a2a
kássia kellem <kassiakellem4@gmail.com>
2024-10-28 19:19:46 -0300

    Componente `Afro` que exibe título como link e descrição estilizados

--- src/Afro.jsx
+++ src/Afro.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Styles from "./Style.module.css"; // Import css
 
-function Afro({ nome, descricao, link }) { //props
+
+function Afro({ titulo, descricao, link }) { //props
   return (
     <div className={Styles.itemResultado}> 
-      <h2><a href={link} target="_blank">{nome}</a></h2>
+      <h2><a href={link} target="_blank" rel="noopener noreferrer">{titulo}</a></h2>
       <p className={Styles.descricaoMeta}>{descricao}</p>
     </div>
   );
