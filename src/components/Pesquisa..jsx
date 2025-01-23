aecb28661a784c2b4ecdf1249ec8ce657a4779c7
kássia kellem <kassiakellem4@gmail.com>
2025-01-23 20:27:26 -0300

    projeto atualizado

--- src/components/Pesquisa..jsx
+++ src/components/Pesquisa..jsx
@@ -4,15 +4,16 @@ import Styles from "../Style.module.css";
 import AfroCard from "./AfroCard";
 import { useState } from "react";
 
-
 function Pesquisa() {
-    const [pesquisa, setPesquisa] = useState(""); // Estado para armazenar o termo pesquisado
+  const [pesquisa, setPesquisa] = useState(""); // Estado para armazenar o termo pesquisado
   const [filteredData, setFilteredData] = useState([]); // Estado para armazenar os resultados filtrados
-  
+  const [naoMostra, setNaoMostra] = useState(false);
   // Função para lidar com a mudança no input
   const handleInputChange = (e) => {
     setPesquisa(e.target.value);
 
+    // Reseta o estado de "Nenhum resultado encontrado" enquanto o usuário digita
+    setNaoMostra(false);
   };
   // Função para lidar com a pesquisa
   const pesquisado = () => {
@@ -21,42 +22,42 @@ function Pesquisa() {
     );
     setFilteredData(filtered);
 
+    // Define o estado para mostrar "Nenhum resultado encontrado" somente após a pesquisa
+    setNaoMostra(true);
+
     // Limpa o campo de pesquisa
     setPesquisa("");
-    
   };
-  
+
   return (
     <div className={Styles.pesquisa}>
-       <input
+      <input
         type="text"
         placeholder="Digite o nome do afro-brasileiro que você quer conhecer."
         value={pesquisa}
         onChange={handleInputChange}
-       
       />
       <button onClick={pesquisado}>Pesquisa</button>
-   
+
       <div className={Styles.cardContainer}>
-        
-        {filteredData.length > 0 ? (
-          filteredData.map((item) => (
+        {filteredData.length > 0
+          ? filteredData.map((item) => (
             <AfroCard
               key={item.id}
               titulo={item.titulo}
               descricao={item.descricao}
               link={item.link}
+              tags={item.tags}
               imagem={item.imagem}
             />
           ))
-        ) : (pesquisa === "" && (
-          <p className={Styles.descricaoMeta}>Nenhum resultado encontrado.</p>
-        ))}
+          : naoMostra && (
+            <p className={Styles.descricaoMeta}>
+              Nenhum resultado encontrado.
+            </p>
+          )}
       </div>
-
     </div>
-
-  
   );
 }
 export default Pesquisa;
